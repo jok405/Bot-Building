b7e1cbc82033f10af6b8ab9690d6fd560c95459c
jok405 <john.y.kim84@gmail.com>
2019-05-28 10:39:12 -0400

    creating expandable window

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="content">
-        <div class="preview">
-      <div class="preview-content">
-        <div class="top-row">
-          <img :src="selectedRobot.head.src"/>
-        </div>
-        <div class="middle-row">
-          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
-          <img :src="selectedRobot.torso.src"/>
-          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
-        </div>
+    <div class="preview">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src"/>
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+            <img :src="selectedRobot.torso.src"/>
+            <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+          </div>
+      </CollapsibleSection>
         <div class="bottom-row">
           <img :src="selectedRobot.base.src"/>
         </div>
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div class="header">
+            <span v-if="open" @click="open != open" >&#x25B2;
+                Collapse</span>
+                <span v-if="!open" @click="open !=open" >&#x25BC; Expand</span>
+        </div>
+    </div>
+</template>
