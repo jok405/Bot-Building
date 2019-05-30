d6b16d842700a81b7759d75adae11c52faf12b93
jok405 <john.y.kim84@gmail.com>
2019-05-30 12:51:01 -0400

    vuex store state

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,14 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  state: {
+    cart: [],
+
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+  },
 
 });
