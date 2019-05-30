af7b8970dc7fcb564c2e99316926be672730dcc5
jok405 <john.y.kim84@gmail.com>
2019-05-30 15:02:47 -0400

    data retrieval from vuex

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,13 @@ import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
 import SidebarStandard from '../sidebars/SidebarStandard.vue';
 import SidebarBuild from '../sidebars/SidebarBuild.vue';
+import ShoppingCart from '../cart/ShoppingCart.vue';
 
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [{
     path: '/',
     name: 'Home',
@@ -55,5 +57,13 @@ export default new Router({
     name: 'Parts',
     component: PartInfo,
     props: true,
+    beforeEnter(to, from, next) {
+      const isValidId = Number.isInteger(Number(to.params.id));
+      next(isValidId);
+    },
+  }, {
+    path: '/cart',
+    name: 'Cart',
+    component: ShoppingCart,
   }],
 });
